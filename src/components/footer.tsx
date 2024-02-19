42eeeb0b78b22f0db3816eb8915a7a81b8001755
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-02-19 00:43:31 -0300

    chore: increase footer height

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex h-16 items-center justify-center text-sm">
+    <footer className="flex h-20 items-center justify-center text-sm">
       <p>Quare Software &copy; {new Date().getFullYear()}</p>
     </footer>
   )
