0ec1882109a7c3cac81ae1290b44d2f23963620d
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-08-03 11:05:22 -0300

    feat: terms, privacy policy and delete account instructions page

---
+++ src/app/deletar-conta/page.tsx
@@ -0,0 +1,54 @@
+export default function DeleteAccountInstructions() {
+  return (
+    <div className="mx-auto flex max-w-4xl flex-col p-16">
+      <h1 className="mb-4 text-4xl font-bold">Como deletar sua conta</h1>
+
+      <p className="mb-4">
+        Para solicitar a exclusão da sua conta e dos seus dados, por favor,
+        envie um e-mail para{' '}
+        <a
+          href="mailto:appborarachar@gmail.com"
+          className="text-blue-500 hover:underline"
+        >
+          appborarachar@gmail.com
+        </a>{' '}
+        com o assunto &quot;Solicitação de Exclusão de Conta&quot;.
+      </p>
+
+      <p className="mb-4">
+        No corpo do e-mail, por favor, inclua as seguintes informações:
+      </p>
+
+      <ul className="mb-4 ml-6 list-disc">
+        <li className="mb-2">Seu nome de usuário</li>
+        <li className="mb-2">O endereço de e-mail associado à sua conta</li>
+        <li className="mb-2">
+          Uma breve confirmação de que você deseja deletar sua conta e seus
+          dados
+        </li>
+      </ul>
+
+      <p className="mb-4">
+        Após recebermos sua solicitação, iremos processá-la o mais rápido
+        possível. Você receberá um e-mail de confirmação quando sua conta e seus
+        dados forem excluídos.
+      </p>
+
+      <p className="mb-4">
+        Observe que, após a exclusão da sua conta, seus dados não poderão ser
+        recuperados.
+      </p>
+
+      <p className="mb-4">
+        Se você tiver alguma dúvida, entre em contato conosco pelo e-mail{' '}
+        <a
+          href="mailto:appborarachar@gmail.com"
+          className="text-blue-500 hover:underline"
+        >
+          appborarachar@gmail.com
+        </a>
+        .
+      </p>
+    </div>
+  )
+}
---
+++ src/app/politica-de-privacidade/page.tsx
@@ -0,0 +1,247 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="mx-auto flex max-w-4xl flex-col p-16">
+      <h1 className="mb-4 text-3xl font-bold">
+        Política de Privacidade do Aplicativo &quot;Bora Rachar&quot;
+      </h1>
+      <p className="mb-2">Última atualização: 03/08/2024 às 11:00</p>
+
+      <p className="mb-4">
+        A Quare Software (&quot;nós&quot;, &quot;nos&quot; ou &quot;nosso&quot;)
+        desenvolveu o aplicativo móvel &quot;Bora Rachar&quot;
+        (&quot;Aplicativo&quot;). Esta Política de Privacidade tem como objetivo
+        informá-lo sobre quais informações coletamos, como as usamos, com quem
+        as compartilhamos e quais são seus direitos em relação a essas
+        informações quando você usa nosso Aplicativo.
+      </p>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">
+          1. Informações que coletamos
+        </h2>
+        <p className="mb-4">
+          Nós coletamos informações sobre você de diversas formas quando você
+          usa o Aplicativo. Essas informações podem incluir:
+        </p>
+
+        <div className="mb-4">
+          <h3 className="mb-2 text-xl font-bold">
+            a) Informações que você nos fornece diretamente:
+          </h3>
+          <ul className="ml-6 list-disc">
+            <li className="mb-2">
+              Informações da conta: Ao criar uma conta no Aplicativo, você nos
+              fornece informações como nome de usuário, endereço de e-mail e
+              senha. Você também pode optar por fornecer informações adicionais,
+              como sua foto de perfil e número de telefone.
+            </li>
+            <li className="mb-2">
+              Conteúdo do Usuário: Ao usar o Aplicativo, você pode fornecer
+              informações sobre suas formas de pagamento, incluindo pix, dados
+              bancários, carteria de criptomoedas, suas despesas, incluindo
+              descrição, valor, data e participantes.
+            </li>
+            <li className="mb-2">
+              Comunicações: Se você entrar em contato conosco diretamente,
+              poderemos coletar informações como seu nome, endereço de e-mail,
+              número de telefone e o conteúdo da sua mensagem.
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-4">
+          <h3 className="mb-2 text-xl font-bold">
+            b) Informações que coletamos automaticamente:
+          </h3>
+          <ul className="ml-6 list-disc">
+            <li className="mb-2">
+              Informações de uso: Coletamos informações sobre como você usa o
+              Aplicativo, como as funcionalidades que você acessa, os recursos
+              que você usa e a frequência com que você usa o Aplicativo.
+            </li>
+            <li className="mb-2">
+              Informações do dispositivo: Coletamos informações sobre o seu
+              dispositivo, como o sistema operacional, o modelo de hardware, o
+              endereço IP, o identificador exclusivo do dispositivo e outros
+              identificadores de dispositivo.
+            </li>
+            <li className="mb-2">
+              Informações de localização: Se você permitir, poderemos coletar
+              informações sobre sua localização usando GPS, Bluetooth ou Wi-Fi.
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-4">
+          <h3 className="mb-2 text-xl font-bold">
+            c) Informações que coletamos de terceiros:
+          </h3>
+          <p className="ml-6">
+            Podemos receber informações sobre você de plataformas de mídia
+            social de terceiros se você optar por conectar sua conta no
+            Aplicativo a essas plataformas.
+          </p>
+        </div>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">
+          2. Como usamos suas informações
+        </h2>
+        <p className="mb-4">
+          Usamos as informações que coletamos sobre você para os seguintes fins:
+        </p>
+        <ul className="ml-6 list-disc">
+          <li className="mb-2">
+            Fornecer e melhorar o Aplicativo: Usamos suas informações para
+            fornecer e melhorar as funcionalidades do Aplicativo, como criar e
+            gerenciar grupos, adicionar participantes, registrar despesas e
+            calcular a divisão de contas.
+          </li>
+          <li className="mb-2">
+            Personalizar sua experiência: Podemos usar suas informações para
+            personalizar sua experiência no Aplicativo, como exibir conteúdo e
+            recursos relevantes para você.
+          </li>
+          <li className="mb-2">
+            Comunicar com você: Podemos usar suas informações para enviar a você
+            comunicados relacionados ao Aplicativo, como anúncios de serviço,
+            atualizações de recursos e solicitações de feedback.
+          </li>
+          <li className="mb-2">
+            Proteger nossos direitos e os de terceiros: Podemos usar suas
+            informações para proteger nossos direitos, a segurança do Aplicativo
+            e a segurança de nossos usuários e terceiros.
+          </li>
+          <li className="mb-2">
+            Cumprir as obrigações legais: Podemos usar suas informações para
+            cumprir as leis e regulamentações aplicáveis.
+          </li>
+        </ul>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">
+          3. Compartilhamento de suas informações
+        </h2>
+        <p className="mb-4">
+          Não compartilhamos suas informações pessoais com terceiros, exceto nas
+          seguintes circunstâncias:
+        </p>
+        <ul className="ml-6 list-disc">
+          <li className="mb-2">
+            Com o seu consentimento: Podemos compartilhar suas informações com
+            terceiros se você nos der permissão para fazê-lo.
+          </li>
+          <li className="mb-2">
+            Com nossos provedores de serviços: Podemos compartilhar suas
+            informações com terceiros que nos ajudam a fornecer e melhorar o
+            Aplicativo, como provedores de hospedagem, análise e marketing.
+          </li>
+          <li className="mb-2">
+            Por motivos legais: Podemos divulgar suas informações se formos
+            obrigados a fazê-lo por lei ou em resposta a uma ordem judicial ou
+            intimação.
+          </li>
+          <li className="mb-2">
+            Para proteger nossos direitos: Podemos divulgar suas informações
+            para proteger nossos direitos, a segurança do Aplicativo e a
+            segurança de nossos usuários e terceiros.
+          </li>
+        </ul>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">
+          4. Seus direitos de privacidade
+        </h2>
+        <p className="mb-4">
+          Você tem os seguintes direitos em relação às suas informações
+          pessoais:
+        </p>
+        <ul className="ml-6 list-disc">
+          <li className="mb-2">
+            Acesso: Você tem o direito de solicitar acesso às informações
+            pessoais que coletamos sobre você.
+          </li>
+          <li className="mb-2">
+            Correção: Você tem o direito de solicitar a correção de quaisquer
+            informações pessoais sobre você que estejam incorretas.
+          </li>
+          <li className="mb-2">
+            Exclusão: Você tem o direito de solicitar a exclusão de suas
+            informações pessoais, exceto quando formos legalmente obrigados a
+            mantê-las.
+          </li>
+          <li className="mb-2">
+            Oposição: Você tem o direito de se opor ao processamento de suas
+            informações pessoais.
+          </li>
+          <li className="mb-2">
+            Portabilidade de dados: Você tem o direito de receber uma cópia de
+            suas informações pessoais em um formato estruturado, comumente usado
+            e legível por máquina.
+          </li>
+        </ul>
+        <p className="ml-6">
+          Você pode exercer qualquer um desses direitos entrando em contato
+          conosco através das informações de contato fornecidas no final desta
+          Política de Privacidade.
+        </p>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">5. Segurança de dados</h2>
+        <p className="mb-4">
+          Implementamos medidas técnicas e organizacionais apropriadas para
+          proteger as informações pessoais que coletamos contra acesso não
+          autorizado, divulgação, alteração ou destruição. No entanto, nenhum
+          método de transmissão pela Internet ou armazenamento eletrônico é 100%
+          seguro. Portanto, não podemos garantir a segurança absoluta de suas
+          informações.
+        </p>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">6. Menores de idade</h2>
+        <p className="mb-4">
+          O Aplicativo não se destina ao uso por menores de 18 anos. Não
+          coletamos intencionalmente informações pessoais de menores de 18 anos.
+          Se você tiver conhecimento de que um menor nos forneceu informações
+          pessoais, entre em contato conosco e tomaremos as medidas cabíveis
+          para remover essas informações de nossos servidores.
+        </p>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">
+          7. Alterações nesta Política de Privacidade
+        </h2>
+        <p className="mb-4">
+          Podemos atualizar esta Política de Privacidade a qualquer momento. Se
+          fizermos alterações materiais, notificaremos você publicando a
+          Política de Privacidade revisada no Aplicativo. Você é aconselhado a
+          revisar esta Política de Privacidade periodicamente para quaisquer
+          alterações. O uso continuado do Aplicativo após a publicação da
+          Política de Privacidade revisada constituirá sua aceitação de tais
+          alterações.
+        </p>
+      </div>
+
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold">8. Contate-nos</h2>
+        <p>
+          Se você tiver alguma dúvida sobre esta Política de Privacidade, entre
+          em contato conosco pelo e-mail:{' '}
+          <a
+            href="mailto:appborarachar@gmail.com"
+            className="text-blue-800 underline"
+          >
+            appborarachar@gmail.com
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  )
+}
