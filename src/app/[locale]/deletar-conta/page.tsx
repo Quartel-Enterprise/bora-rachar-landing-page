fe60062f56ccd5d9ecbfbf402d7e70281ba55139
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-08-18 19:06:59 -0300

    chore: delete account intl

--- src/app/[locale]/deletar-conta/page.tsx
+++ src/app/[locale]/deletar-conta/page.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from 'next-intl'
 import { unstable_setRequestLocale as unstableSetRequestLocale } from 'next-intl/server'
 
 import { ComponentLocaleProps } from '@/utils/i18nConfig'
@@ -6,56 +7,48 @@ export default function DeleteAccountInstructions({
   params: { locale },
 }: ComponentLocaleProps) {
   unstableSetRequestLocale(locale)
+  const t = useTranslations('DeleteAccount')
 
   return (
     <div className="mx-auto flex max-w-4xl flex-col p-16">
-      <h1 className="mb-4 text-4xl font-bold">Como deletar sua conta</h1>
+      <h1 className="mb-4 text-4xl font-bold">{t('title')}</h1>
 
       <p className="mb-4">
-        Para solicitar a exclusão da sua conta e dos seus dados, por favor,
-        envie um e-mail para{' '}
-        <a
-          href="mailto:appborarachar@gmail.com"
-          className="text-blue-500 hover:underline"
-        >
-          appborarachar@gmail.com
-        </a>{' '}
-        com o assunto &quot;Solicitação de Exclusão de Conta&quot;.
+        {t.rich('requestInstructions', {
+          email: (chunk) => (
+            <a
+              href="mailto:appborarachar@gmail.com"
+              className="text-blue-500 hover:underline"
+            >
+              {chunk}
+            </a>
+          ),
+        })}
       </p>
 
-      <p className="mb-4">
-        No corpo do e-mail, por favor, inclua as seguintes informações:
-      </p>
+      <p className="mb-4">{t('emailInstructions')}</p>
 
       <ul className="mb-4 ml-6 list-disc">
-        <li className="mb-2">Seu nome de usuário</li>
-        <li className="mb-2">O endereço de e-mail associado à sua conta</li>
-        <li className="mb-2">
-          Uma breve confirmação de que você deseja deletar sua conta e seus
-          dados
-        </li>
+        <li className="mb-2">{t('usernameInstruction')}</li>
+        <li className="mb-2">{t('emailInstruction')}</li>
+        <li className="mb-2">{t('confirmationInstruction')}</li>
       </ul>
 
-      <p className="mb-4">
-        Após recebermos sua solicitação, iremos processá-la o mais rápido
-        possível. Você receberá um e-mail de confirmação quando sua conta e seus
-        dados forem excluídos.
-      </p>
+      <p className="mb-4">{t('processingConfirmation')}</p>
+
+      <p className="mb-4">{t('dataRecoveryWarning')}</p>
 
       <p className="mb-4">
-        Observe que, após a exclusão da sua conta, seus dados não poderão ser
-        recuperados.
-      </p>
-
-      <p className="mb-4">
-        Se você tiver alguma dúvida, entre em contato conosco pelo e-mail{' '}
-        <a
-          href="mailto:appborarachar@gmail.com"
-          className="text-blue-500 hover:underline"
-        >
-          appborarachar@gmail.com
-        </a>
-        .
+        {t.rich('contactInformation', {
+          email: (chunk) => (
+            <a
+              href="mailto:appborarachar@gmail.com"
+              className="text-blue-500 hover:underline"
+            >
+              {chunk}
+            </a>
+          ),
+        })}
       </p>
     </div>
   )
