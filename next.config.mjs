dd3d121c5a2338cd97cfc1ce668f956b3633e82b
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-09-26 23:13:51 -0300

    chore: redirect well know to non www

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,21 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  redirects: async () => {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.borarachar.com',
+          },
+        ],
+        destination: 'https://borarachar.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withNextIntl(nextConfig)
